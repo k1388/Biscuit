a5c163182bd00b15e3e237a8214baf1e664922e9
otee <kanhongxia2@163.com>
2025-06-24 18:17:25 +0800

    Add Events and EventDispatcher

--- Biscuit/src/Biscuit/Event/Event.h
+++ Biscuit/src/Biscuit/Event/Event.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "../Core.h"
 #include <string>
+#include <functional>
 
 namespace Biscuit
 {
-
+	
 	enum class EventType
 	{
 		None = 0,
@@ -48,4 +49,31 @@ namespace Biscuit
 		bool m_Handled = false;
 	};
 
+	class EventDispatcher
+	{
+		template<typename T>
+		using EventFn = std::function<bool<T&>>;
+	public:
+
+		EventDispatcher(Event& e):m_Event(e){}
+
+		template<typename T>
+		bool Dispatch(EventFn<T&> func)
+		{
+			if (m_Event.GetEventType() == T::GetStaticType())
+			{
+				m_Event.m_Handled = func(*(T*)m_Event);
+				return true;
+			}
+			return false;
+		}
+
+	private:
+		Event& m_Event;
+	};
+
+	inline std::ostream& operator << (std::ostream& os, const Event& e)
+	{
+		return os << e.ToString();
+	}
 }
