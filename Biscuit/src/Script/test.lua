74c9d7bb509c74b4720b2093eacbeaeb3ce5ff03
otee <kanhongxia2@163.com>
2025-10-31 12:02:52 +0800

    LuaSys:实现了访问sprite属性，实现了几个钩子

--- Biscuit/src/Script/test.lua
+++ Biscuit/src/Script/test.lua
@@ -1,6 +1,8 @@
 ﻿test = 
 {
-    update = function(self)
-       print("updated!")
+    speed = 300
+    update = function(self, dt)
+       self.transform.x += Input.horizontal * speed * dt
+       self.transform.y += Input.vertical * speed * dt
     end
 }
